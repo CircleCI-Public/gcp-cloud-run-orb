465735c34323c03629278048417beb25fc64ccb9
Kyle Tryon <Kyle.Tryon@outlook.com>
2019-10-25 11:58:05 -0400

    hard code environment variables in container for testing.

--- sample/index.js
+++ sample/index.js
@@ -4,11 +4,9 @@ const app = express()
 app.get('/', (req, res) => {
   console.log('Hello world received a request.')
 
-  const target = process.env.TARGET || 'World'
-  res.send(`Hello ${target}!`)
+  res.send(`Hello World!`)
 })
 
-const port = process.env.PORT || 8080
-app.listen(port, () => {
+app.listen(8080, () => {
   console.log('Hello world listening on port', port)
 })
