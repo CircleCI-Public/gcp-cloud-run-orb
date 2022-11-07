230c7f5268d01babf58a79978246358184dc66f9
Jaryt <jarytbustard@gmail.com>
2022-11-07 15:15:39 -0500

    refactor: begin script migration

---
+++ src/scripts/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# eval env vars
+GCP_SOURCE=$(eval "echo $ORB_EVAL_SOURCE")
+GCP_TAG=$(eval "echo $ORB_EVAL_TAG")
+GCP_CONFIG=$(eval "echo $ORB_EVAL_CONFIG")
+GCP_ARGS=$(eval "echo $ORB_EVAL_ARGS")
+
+# if no source parameter is set, check for a Dockerfile and fail if none is found.
+if [ ! -f Dockerfile ] && [ -z "$GCP_SOURCE" ]; then
+    echo "No Dockerfile in $(pwd). You may need to specify a source location."
+    exit 1
+fi
+
+
+args=()
+
+if [ -n "$GCP_TAG" ]; then
+  args+=(--tag "$GCP_TAG")
+fi
+
+if [ -n "$GCP_CONFIG" ]; then
+  args+=(--config "$GCP_CONFIG")
+fi
+
+set -x
+gcloud builds submit "${args[@]}" "$GCP_ARGS"
+set +x
